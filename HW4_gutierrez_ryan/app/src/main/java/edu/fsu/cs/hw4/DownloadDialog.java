3093d5ba8fb6540d867b215451dc453980613ab1
pyrokuna <31514534+pyrokuna@users.noreply.github.com>
2020-02-29 14:39:18 -0500

    Finished base MainActivity Layout and DialogBox
    Need to work on getting the download to actually work and then getting the media service to work properly as well.

---
+++ HW4_gutierrez_ryan/app/src/main/java/edu/fsu/cs/hw4/DownloadDialog.java
@@ -0,0 +1,52 @@
+package edu.fsu.cs.hw4;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import edu.fsu.cs.hw4.R;
+
+public class DownloadDialog extends AppCompatDialogFragment {
+    private TextView et_url;
+    private Button bt_download;
+    private DownloadDialogListener listener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.download_dialog, null);
+
+        builder.setView(view);
+
+        et_url = (TextView) view.findViewById(R.id.url);
+        bt_download = (Button) view.findViewById(R.id.button_download);
+
+        bt_download.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String url = et_url.getText().toString();
+                listener.applyURL(url);
+            }
+        });
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        listener = (DownloadDialogListener) context;
+    }
+
+    public interface DownloadDialogListener {
+        void applyURL(String url);
+    }
+}
--- HW4_gutierrez_ryan/app/src/main/java/edu/fsu/cs/hw4/MainActivity.java
+++ HW4_gutierrez_ryan/app/src/main/java/edu/fsu/cs/hw4/MainActivity.java
@@ -2,11 +2,18 @@ package edu.fsu.cs.hw4;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+// Used this as reference for Overflow menu: https://www.youtube.com/watch?v=oh4YOj9VkVE
+// Used this as reference for Download Dialog: https://www.youtube.com/watch?v=ARezg1D9Zd0
+
+public class MainActivity extends AppCompatActivity implements DownloadDialog.DownloadDialogListener {
 
     private static final String TAG = MainActivity.class.getCanonicalName();
+    private String URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +22,38 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO setup UI
 
+
         // TODO Do we need to handle any Extras?
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.of_download:
+                // whatever would go in opendialog()
+                DownloadDialog downloadDialog = new DownloadDialog();
+                downloadDialog.show(getSupportFragmentManager(), "Download Dialog");
+                return true;
+            case R.id.of_exit:
+                finish();
+                System.exit(0);
+                // TODO still need to make sure MediaPlayer service stops and the download service does not stop
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void applyURL(String url) {
+        URL = url;
+        //Toast.makeText(this, URL, Toast.LENGTH_LONG).show();
     }
 
     // onClick method for Play button
@@ -32,5 +70,4 @@ public class MainActivity extends AppCompatActivity {
     public void stop(View v) {
         // TODO stop MediaPlayer
     }
-
 }
